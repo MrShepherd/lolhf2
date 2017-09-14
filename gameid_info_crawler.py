4962c09731e34f2eb0aeddf5023065c25e37bd5e
mr_shepherd <heimd4ll@yeah.net>
2017-09-14 17:32:51 +0800

    refactor crawler to multi process

--- gameid_info_crawler.py
+++ gameid_info_crawler.py
@@ -1,8 +1,8 @@
 import time
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-
+from multiprocessing.dummy import Pool
 import dbhandler
 
 
@@ -16,6 +16,7 @@ class GameIDInfoCrawler(object):
         self.gameids_ladder = set()
         self.gameids_add = set()
         self.gameid_info = []
+        self.invalid_gameids = set()
 
     def crawl_idmapping_info(self):
         # service_args = ['--load-images=false']
@@ -57,6 +58,46 @@ class GameIDInfoCrawler(object):
         print(self.idmapping_data)
         return self.idmapping_data
 
+    def crawl_gameid_info_by_search(self, gameid):
+        browser = webdriver.PhantomJS(executable_path="/opt/phantomjs/bin/phantomjs")
+        browser.set_page_load_timeout(15)
+        try:
+            browser.get(self.gameid_info_url)
+        except TimeoutException:
+            browser.execute_script("window.stop()")
+        form = browser.find_element_by_xpath("//div[@class='PageHeaderWrap']//form[@class='FormItem']//input[@class='Input']")
+        form.clear()
+        form.send_keys(gameid)
+        try:
+            form.send_keys(Keys.RETURN)
+        except TimeoutException:
+            browser.execute_script("window.stop()")
+        time.sleep(3)
+        browser.execute_script("window.stop()")
+        try:
+            tmp_dict = {}
+            tmp_dict['rank'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'Rank') and contains(@class,'Cell')]").text
+            tmp_dict['game_id'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'SummonerName')]/a").text
+            tmp_dict['link'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'SummonerName')]/a").get_attribute("href")
+            tmp_dict['tier'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'TierRank')]").text
+            tmp_dict['lp'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'LP')]").text.split()[0].replace(',', '')
+            tmp_dict['total_win'] = browser.find_element_by_xpath(
+                "//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'RatioGraph')]//div[contains(@class,'Text') and contains(@class,'Left')]").text
+            tmp_dict['total_lose'] = browser.find_element_by_xpath(
+                "//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'RatioGraph')]//div[contains(@class,'Text') and contains(@class,'Right')]").text
+            tmp_dict['total_win_ratio'] = browser.find_element_by_xpath(
+                "//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'RatioGraph')]//span[@class='WinRatio']").text.replace('%', '')
+            print(tmp_dict)
+            self.gameid_info.append(tmp_dict)
+        except NoSuchElementException:
+            print('invalid gameid:', gameid)
+            self.invalid_gameids.add(gameid)
+        self.gameids_add.remove(gameid)
+        print('gameids left:', len(self.gameids_add))
+        browser.quit()
+        browser.close()
+        return 'ok'
+
     def crawl_gameid_info(self):
         browser = webdriver.PhantomJS(executable_path="/opt/phantomjs/bin/phantomjs")
         # browser = webdriver.Chrome()
@@ -67,7 +108,7 @@ class GameIDInfoCrawler(object):
         except TimeoutException:
             browser.execute_script("window.stop()")
         count = 1
-        while count <= 5:
+        while count <= 20:
             js = "document.body.scrollTop=%d000" % (count * 500)
             browser.execute_script(js)
             time.sleep(1)
@@ -95,24 +136,15 @@ class GameIDInfoCrawler(object):
         self.gameids_add = (gameids_db - self.gameids_ladder) | self.gameids_add
         print('number of gameids still need to be crawled:', len(self.gameids_add))
         print(self.gameids_add)
-        try:
-            browser.get(self.gameid_info_url)
-        except TimeoutException:
-            browser.execute_script("window.stop()")
-        form = browser.find_element_by_xpath("//div[@class='PageHeaderWrap']//form[@class='FormItem']//input[@class='Input']")
-        browser.set_page_load_timeout(15)
-        for gameid in self.gameids_add:
-            form.clear()
-            form.send_keys(gameid)
-            try:
-                form.send_keys(Keys.RETURN)
-            except TimeoutException:
-                browser.execute_script("window.stop()")
-            tmp_dict = {}
-            self.gameid_info.append(tmp_dict)
-            time.sleep(5)
         browser.close()
         browser.quit()
+        while (len(self.gameids_add) != 0):
+            pool = Pool(8)
+            pool.map(self.crawl_gameid_info_by_search, self.gameids_add)
+            pool.close()
+            pool.join()
+        print('invalid gameids found:', len(self.invalid_gameids))
+        print(self.invalid_gameids)
         print('number of gameids crawled:', len(self.gameid_info))
         print(self.gameid_info)
         return self.gameid_info
--- test/test_gameIDInfoCrawler.py
+++ test/test_gameIDInfoCrawler.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from gameid_info_crawler import GameIDInfoCrawler
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 # from selenium.webdriver.common.keys import Keys
@@ -22,12 +24,40 @@ class TestGameIDInfoCrawler(TestCase):
     def test_crawl_gameid(self):
         test_crawler = GameIDInfoCrawler()
         browser = webdriver.Firefox()
-        browser.set_page_load_timeout(30)
+        browser.set_page_load_timeout(15)
         try:
             browser.get(test_crawler.gameid_info_url)
         except TimeoutException:
             browser.execute_script("window.stop()")
         print('length of html:', len(browser.page_source))
+        test_gameids = set(['M M M M O O O O', 'SSG 메르시', '열심히할게요오', 'SIlde', 'Nox OaO', 'Wo jo lan A', 'RPG Evi'])
+        browser.set_page_load_timeout(10)
+        for gameid in test_gameids:
+            form = browser.find_element_by_xpath("//div[@class='PageHeaderWrap']//form[@class='FormItem']//input[@class='Input']")
+            form.clear()
+            form.send_keys(gameid)
+            try:
+                form.send_keys(Keys.RETURN)
+            except TimeoutException:
+                browser.execute_script("window.stop()")
+            time.sleep(5)
+            browser.execute_script("window.stop()")
+            try:
+                tmp_dict = {}
+                tmp_dict['rank'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'Rank') and contains(@class,'Cell')]").text
+                tmp_dict['game_id'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'SummonerName')]/a").text
+                tmp_dict['link'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'SummonerName')]/a").get_attribute("href")
+                tmp_dict['tier'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'TierRank')]").text
+                tmp_dict['lp'] = browser.find_element_by_xpath("//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'LP')]").text.split()[0].replace(',', '')
+                tmp_dict['total_win'] = browser.find_element_by_xpath(
+                    "//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'RatioGraph')]//div[contains(@class,'Text') and contains(@class,'Left')]").text
+                tmp_dict['total_lose'] = browser.find_element_by_xpath(
+                    "//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'RatioGraph')]//div[contains(@class,'Text') and contains(@class,'Right')]").text
+                tmp_dict['total_win_ratio'] = browser.find_element_by_xpath(
+                    "//table[@class='LadderRankingTable']//tr[contains(@class,'Selected')]//td[contains(@class,'RatioGraph')]//span[@class='WinRatio']").text.replace('%', '')
+                print(tmp_dict)
+            except NoSuchElementException:
+                print('bad gameid')
         # browser.close()
         browser.quit()
         self.assertTrue(1 == 1)
